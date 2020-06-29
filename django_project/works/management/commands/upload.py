045e17e285a544de2d0941e8fb303249ce041b76
Viacheslav Kukushkin <vy.kukushkin@gmail.com>
2020-06-29 23:44:45 +0300

    Changed data model: added separate contributor model, added deduplication

--- django_project/works/management/commands/upload.py
+++ django_project/works/management/commands/upload.py
@@ -1,7 +1,24 @@
 from django.core.management.base import BaseCommand
 from tqdm import tqdm
 import csv
-from works.models import Metadata
+from works.models import Contributor, Metadata
+from django.db import models
+
+
+def insert_and_get_contributors(contributors_names):
+    result = []
+    for name in contributors_names:
+        try:
+            contributor = Contributor.objects.get(name=name)
+        except models.ObjectDoesNotExist:
+            contributor = Contributor(name=name)
+            contributor.save()
+        result.append(contributor)
+    return result
+
+
+def to_set(contributors):
+    return {c.name for c in contributors}
 
 
 class Command(BaseCommand):
@@ -11,10 +28,39 @@ class Command(BaseCommand):
         input_filepath = options['csv_file']
         verbose = options['verbosity'] > 0
         with open(input_filepath, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = csv.DictReader(csvfile, quoting=csv.QUOTE_NONE)
             for row in tqdm(reader, disable=not verbose):
-                m = Metadata(title=row['title'], contributors=row['contributors'], ISWC=row['iswc'])
-                m.save()
+                title = row['title']
+                contributors_names = row['contributors'].split('|')
+                ISWC = row['iswc'] or None
+                contributors = insert_and_get_contributors(contributors_names)
+                contributors_names = to_set(contributors)
+
+                try:
+                    if ISWC:
+                        meta = Metadata.objects.get(ISWC=ISWC)
+                    else:
+                        raise models.ObjectDoesNotExist()
+                except models.ObjectDoesNotExist:
+                    meta = Metadata(title=title, ISWC=ISWC)
+                    meta.save()
+
+                for contributor in contributors:
+                    meta.contributors.add(contributor)
+                meta.save()
+
+                similar_metas = Metadata.objects.filter(title=title)
+                contributors_names = set(contributors_names)
+                for similar_meta in similar_metas:
+                    if meta == similar_meta:
+                        continue
+                    similar_contributors = similar_meta.contributors.all()
+                    if to_set(similar_contributors) & contributors_names:
+                        for similar_contributor in similar_contributors:
+                            if similar_contributor.name not in contributors_names:
+                                meta.contributors.add(similar_contributor)
+                        similar_meta.delete()
+                meta.save()
 
     def add_arguments(self, parser):
         parser.add_argument('csv_file', help='input csv file with headers')
--- django_project/works/models.py
+++ django_project/works/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 
+class Contributor(models.Model):
+    name = models.CharField(max_length=128, unique=True, db_index=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Metadata(models.Model):
-    title = models.TextField()
-    contributors = models.TextField()
-    ISWC = models.TextField()
+    title = models.CharField(max_length=128, db_index=True)
+    contributors = models.ManyToManyField(Contributor)
+    ISWC = models.CharField(max_length=12, unique=True, null=True, db_index=True)
 
     def __str__(self):
         return f"{self.ISWC}: <<{self.title}>> by {self.contributors}"
